6817ad2ec794a9630ce690e7349c9e1b836609ee
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-01-31 00:18:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
 import fetchCountries from './js/fetchCountries';
 import {
   createCountryListMarkup,
   createCountryProfileMarkup,
 } from './js/createMarkupFunctions';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
